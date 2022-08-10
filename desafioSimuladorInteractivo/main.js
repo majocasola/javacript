387b7f70525f0514ff52e61917496e2666d9a2e8
Ramiro Fazio Dattoli <70992145+ramirofazio@users.noreply.github.com>
2022-08-10 19:04:33 -0300

    Sugerencias D2!

--- desafioSimuladorInteractivo/main.js
+++ desafioSimuladorInteractivo/main.js
@@ -1,6 +1,6 @@
 // calculo de la compra
 
-let ingreso= prompt("Por favor ingrese el monto de su compra");
+let ingreso= parseInt(prompt("Por favor ingrese el monto de su compra")); //ParseInt te trasforma el string a Number, para que funciona la comparacion, no va a hacer falta el parseFloat
 
 function costo(valor) {
     let resultado = valor;
@@ -8,20 +8,19 @@ function costo(valor) {
     if (valor > 1000) {
         resultado = valor * 0.75;
         alert("Se le realizo un 25% de descuento, su monto a pagar es de: " + resultado);
-
-    } else if (valor == 1000) {
+    } else if (valor === 1000) { //Mejor usar "===" para las comparacines! 
         resultado = valor * 0.90;
         alert("Se le realizo un 10% de descuento, su monto a pagar es de: " + resultado);
     } else if (valor < 1000) {
         alert("Su monto a pagar es de: " + resultado);
     } else {
-        alert("El valor ingresado es incorrecto, recuerde que debe ingresar numeros positivos mayores a cero");
+        alert("El valor ingresado es incorrecto, recuerde que debe ingresar numeros positivos mayores a cero"); //Esto es asi? No lo veo validado en ningun lado
     }
 
     console.log(`debe pagar $${resultado}`)
 
 }
-costo(parseFloat(ingreso));
+costo(ingreso);
 
 
 
